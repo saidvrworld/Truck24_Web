af2e180d940649caf8b9b2fc1009329c41cab777
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-03 08:48:56 +0500

    add TeleBot requirements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import telebot
 from flask import Flask, request
 
+
 token = "309803225:AAEfkOtjUfLCTSHicJD05uy7AvilTCkzOYs"
 
 bot = telebot.TeleBot(token)
@@ -16,7 +17,7 @@ def start(message):
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-@server.route("/bot", methods=['POST'])
+#@server.route("/bot", methods=['POST'])
 def getMessage(request1=None):
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
