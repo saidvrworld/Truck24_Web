e508d73b3539e44439d67be3a602473026a33cde
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-03 19:43:25 +0500

    add TeleBot requirements

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
 import telebot
-from flask import Flask, request
+#from flask import Flask, request
 
 
 token = "309803225:AAEfkOtjUfLCTSHicJD05uy7AvilTCkzOYs"
 
 bot = telebot.TeleBot(token)
 
-server = Flask(__name__)
+#server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,10 +19,10 @@ def echo_message(message):
 
 #@server.route("/bot", methods=['POST'])
 def getMessage(request1=None):
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    #bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-@server.route("/bot/hook")
+#@server.route("/bot/hook")
 def webhook(request):
     bot.remove_webhook()
     bot.set_webhook(url="https://favorittaxi.herokuapp.com/bot")
