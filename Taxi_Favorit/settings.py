1db1f156b863d87fcb2055e4cd8cbf6f1c54c158
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-04 10:39:32 +0500

    Test GetUpdate

--- Taxi_Favorit/settings.py
+++ Taxi_Favorit/settings.py
@@ -117,7 +117,7 @@ DATABASES['default'].update(db_from_env)
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
