9d47555554ed8fc74cdd8ee27a6c634915e19f8d
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-03-05 16:11:35 +0500

    Start

--- umsbot/BotManager.py
+++ umsbot/BotManager.py
@@ -8,6 +8,8 @@ class BotManager:
 
     price_list = "💲  (Кобальт, Ласетти): 1000 сум/км, минимум 8000 сум (5 км. включительно).\n\n💲  Доставка: 1000 сум/км, минимум 7000 сум (5 км. включительно).\n\n💲  Перегон: 2000 сум/км, минимум 20000 сум (5 км. включительно).\n\n💲 Ожидание: 300 сум/минуту. 5 минут бесплатно."
 
+    balance = {"баланс":"*100#","последняя оплата":"*171*1*2#","мой расход":"*171*1*3#","мой номер":"*150#","все мои номера":"*151#"}
+
     bot = None
 
     def __init__(self,bot):
@@ -31,6 +33,9 @@ class BotManager:
 
             if call_data == "start":
                 self.ShowMainMenu(message)
+            elif(call_data in self.balance.keys()):
+                self.bot.send_contact(current_chat_id, self.balance[call_data], call_data)
+
 
 
 
@@ -64,4 +69,25 @@ class BotManager:
         keyboard.add(button7)
 
         self.bot.send_message(current_chat_id, "Главное Меню", reply_markup=keyboard)
+
+
+    def Balance(self,message):
+        current_chat_id = message["chat"]["id"]
+
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        button1 = telebot.types.InlineKeyboardButton(text="БАЛАНС", callback_data="баланс")
+        button2 = telebot.types.InlineKeyboardButton(text="ПОСЛЕДНЯЯ ОПЛАТА", callback_data="последняя оплата")
+        button3 = telebot.types.InlineKeyboardButton(text="МОЙ РАСХОД", callback_data="мой расход")
+        button4 = telebot.types.InlineKeyboardButton(text="МОЙ НОМЕР", callback_data="мой номер")
+        button5 = telebot.types.InlineKeyboardButton(text="ВСЕ МОИ НОМЕРА", callback_data="все мои номера")
+
+
+        keyboard.add(button1)
+        keyboard.add(button2)
+        keyboard.add(button3)
+        keyboard.add(button4)
+        keyboard.add(button5)
+
+        self.bot.send_message(current_chat_id, "Вы выбрали МОЙ БАЛАНС", reply_markup=keyboard)
+
 
