7839993274e73b08fc7651321651f8b7889ebae7
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-04-27 23:24:13 +0500

    Start

--- track24/views.py
+++ track24/views.py
@@ -106,7 +106,7 @@ def AddOrder(request):
     long_to = 69.253581
     notes = request.POST["notes"]
     date = "16.05.2017"
-    postData = {"token":token,"carTypeId":carTypeId,"lat_from":str(lat_from),"long_from":str(long_from),"lat_to":str(lat_to),"long_to":str(long_to),"notes":notes,"date":date}
+    postData = {"token":token,"carTypeId":str(carTypeId),"lat_from":str(lat_from),"long_from":str(long_from),"lat_to":str(lat_to),"long_to":str(long_to),"notes":notes,"date":date}
 
     dataBody = MakeRequest(urlPath=url,post_data=postData)[0]
     request.session["customer_token"] = dataBody["token"]
